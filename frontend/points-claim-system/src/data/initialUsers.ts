3f639e9352364f8900a4989f8ba483730dcac62b
Sudipta Mondal <msudipta857@gmail.com>
2025-07-19 09:58:38 +0530

    chnage in initUser

--- frontend/points-claim-system/src/data/initialUsers.ts
+++ frontend/points-claim-system/src/data/initialUsers.ts
@@ -1,14 +1,8 @@
 import type {User} from '../types'
 
 export const initialUsers: User[] = [
-  { id: '1', name: 'Alice Johnson',email:"alice@gmail.com",totalPoints:0 },
-  { id: '2', name: 'Bob Smith', email:"bob@gmail.com" ,totalPoints:0 },
-  { id: '3', name: 'Charlie Brown',email:"charlie@gmail.com", totalPoints:0},
-  { id: '4', name: 'Diana Prince',email:"diana@gmail.com",totalPoints:0  },
-  { id: '5', name: 'Edward Norton',email:"edward@gmail.com" ,totalPoints:0},
-  { id: '6', name: 'Fiona Green' ,email:"fiona@gmail.com",totalPoints:0 },
-  { id: '7', name: 'George Wilson',email:"george@gmail.com",totalPoints:0  },
-  { id: '8', name: 'Hannah Davis' ,email:"hannah@gmail.com" ,totalPoints:0},
-  { id: '9', name: 'Ian Mitchell',email:"ian@gmail.com" ,totalPoints:0 },
-  { id: '10', name: 'Julia Roberts',email:"julia@gmail.com" ,totalPoints:0 },
+  { id: '111', name: 'sudip',email:"sk@s.com",totalPoints:135  },
+  { id: 'fg6', name: 'tara' ,email:"tara@m.com" ,totalPoints:122},
+  { id: '1e4', name: 'raf',email:"rs@s.com" ,totalPoints:120 },
+  { id: '1', name: 'Alice Jhonson',email:"alice@gmail.com" ,totalPoints:0 },
 ];
